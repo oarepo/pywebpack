31ecf717b533748b8af429f3d75a2cbd9377e78b
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 13:51:49 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2017 CERN.
-# SPDX-FileCopyrightText: 2022 Graz University of Technology.
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""Webpack integration layer for Python."""
-
-from setuptools import setup
-
-setup()
